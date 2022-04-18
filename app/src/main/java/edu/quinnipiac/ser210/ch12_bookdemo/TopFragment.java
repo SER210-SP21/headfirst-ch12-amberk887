55de64443562a7f9ee8e1b6a0b2ba0169ee48972
amberk887 <amber.kusma@quinnipiac.edu>
2022-04-17 23:14:00 -0400

    ch 12

---
+++ app/src/main/java/edu/quinnipiac/ser210/ch12_bookdemo/TopFragment.java
@@ -0,0 +1,22 @@
+package edu.quinnipiac.ser210.ch12_bookdemo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TopFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_top, container, false);
+    }
+}
